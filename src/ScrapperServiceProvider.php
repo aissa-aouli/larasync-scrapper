27755f879aa1b470543938d9c066523981507bf0
Aissa <aissa.aouli@gmail.com>
2020-01-18 23:29:22 +0100

    Initial Commit

--- src/ScrapperServiceProvider.php
+++ src/ScrapperServiceProvider.php
@@ -33,14 +33,12 @@ class ScrapperServiceProvider extends ServiceProvider
             ], 'scrapper');
         }
 
-        dd('boot');
 
     }
 
     public function register()
     {
 
-        dd('register');
         // Load the config file and merge it with the user's (should it get published)
 //        $this->mergeConfigFrom( __DIR__.'/config/scrapper.php', 'scrapper');
 
@@ -57,11 +55,12 @@ class ScrapperServiceProvider extends ServiceProvider
      */
     protected function bootForConsole()
     {
-        dd('bootForConsole');
+
         $this->publishes([
                 __DIR__.'/config/scrapper.php' => config_path('scrapper.php'),
             ]
             , 'scrapper');
+
     }
 
 }

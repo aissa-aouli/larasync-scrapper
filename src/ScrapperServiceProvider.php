ed7620fa226cad04c2bd2e063bf7ae34d8e5661c
Aissa <aissa.aouli@gmail.com>
2020-01-18 17:25:01 +0100

    Initial Commit

--- src/ScrapperServiceProvider.php
+++ src/ScrapperServiceProvider.php
@@ -16,10 +16,21 @@ class ScrapperServiceProvider extends ServiceProvider
         ]
             , 'scrapper');
 
-        $this->publishes([
-            __DIR__.'/Database/migrations/' => database_path('migrations')
-        ], 'scrapper');
+//        $this->publishes([
+//            __DIR__.'/Database/migrations/' => database_path('migrations')
+//        ], 'scrapper');
 
+        if (! class_exists('CreateProxiesTable')) {
+            $this->publishes([
+                __DIR__.'/Database/migrations/create_proxies_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_proxies_table.php'),
+            ], 'scrapper');
+        }
+
+        if (! class_exists('CreateUserAgentsTable')) {
+            $this->publishes([
+                __DIR__.'/Database/migrations/create_user_agents_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_user_agent_table.php'),
+            ], 'scrapper');
+        }
     }
 
     public function register()
